16d4383501ed1c201fd89eb21d0477ddfa17982f
CongjieHe <congjiehe95@gmail.com>
2021-11-10 02:10:35 +0800

    version 0.1.0 完善requirements.txt和mail_center.py

--- utils/mail_center.py
+++ utils/mail_center.py
@@ -12,9 +12,8 @@ from email.header import Header
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+import unittest
 from config import *
-
 from config.email_config import MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASS, MAIL_RECEIVER
 
 socket.socket = socks.socksocket
@@ -42,6 +41,18 @@ class EmailCenter:
             msgImage.add_header('Content-ID', title)
             ss = ss + """<br><img src="cid:%s"></br>\n""" % title
             msg.attach(msgImage)
+        html = """
+         <html>
+             <head></head>
+             <body>
+                 <p>This email was sent by env: %s ：<br>
+                     %s              
+                 </p>
+             </body>
+         </html>
+         """ % (RUN_ENV, ss)
+        message = MIMEText(html, "html", "utf-8")
+        msg.attach(message)
         try:
             smtpObj = smtplib.SMTP_SSL(MAIL_HOST)
             smtpObj.connect('pop.exmail.qq.com', 465)
@@ -52,3 +63,40 @@ class EmailCenter:
             logger_error.error(str(e))
         else:
             logger_info.info("send email success")
+
+
+class TestMailSend(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        warnings.filterwarnings(action="ignore", message="unclosed", category=ResourceWarning)
+        print("Begin testing email sender")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print("End to test email sender")
+
+    def test_send_email(self):
+        subject = 'Python SMTP 邮件测试'
+        email = EmailCenter()
+        message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
+        message['From'] = Header("数据服务器", 'utf-8')
+        message['To'] = Header("测试", 'utf-8')
+        message['Subject'] = Header(subject, 'utf-8')
+
+        try:
+            # pass
+            smtpObj = smtplib.SMTP_SSL(email.mail_host)
+            smtpObj.connect(email.mail_host, email.mail_port)
+            smtpObj.login(email.mail_user, email.mail_pass)
+            smtpObj.sendmail(email.mail_user, email.mail_rec, message.as_string())
+            smtpObj.quit()
+            smtpObj.close()
+        except smtplib.SMTPException as e:
+            print(e)
+            print("Unknown error happened, send email failed")
+        else:
+            print("send email success")
+
+
+if __name__ == '__main__':
+    unittest.main()
