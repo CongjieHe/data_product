6bec8b83935e5b9b566f226f97bdc8c66d25337d
CongjieHe <congjiehe95@gmail.com>
2021-11-16 10:45:49 +0800

    version 0.3.0 增加Alphas101因子计算模块

--- Alphas_101/alphas_101.py
+++ Alphas_101/alphas_101.py
@@ -10,3 +10,16 @@ from numpy import sign
 
 import pandas as pd
 import time
+
+class Alphas_101:
+    def __init__(self, data, factor_id, save=False):
+        self.high = data['high']
+        self.low = data['low']
+        self.close = data['close']
+        self.open = data['open']
+        self.volume = data['volume']
+        self.returns = self.close.pct_change()
+        self.vwap = (data['open'] + data['high'] + data['low'] + data['close']) / 4
+
+    def alpha001(self):
+        inner = self.close.copy(deep=True)
